7162a82899c594466f2f314dd1b4a5cc7e1da030
Charles Rumford <charlesr@tallwireless.com>
2019-07-10 23:53:42 -0400

    Adding the configure routing

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ app = Flask("ConFi")
 
 @app.route('/')
 def create():
-    url_for("static", filename="css/tent.css")
-    url_for("static", filename="btv-logo.png")
-    content = render_template("create.tpl")
     data = { "title"   : "Wifi Registration",
              "subtitle": "WiFi Registration",
              "content" : render_template("create.tpl"),
            }
     return render_template("main.tpl",**data)
 
-@app.route('/hello3/')
-def hello_world2():
-        return 'Hedlo, World!'
+@app.route('/configure')
+def configure():
+    data = { "title"   : "Configuration",
+             "subtitle": "WiFi Configuration",
+             "content" : render_template("configure.tpl"),
+           }
+    return render_template("main.tpl",**data)

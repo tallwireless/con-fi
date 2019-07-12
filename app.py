d65a78f187d108ac21b50e9f16141fd7011347bc
Charles Rumford <charlesr@tallwireless.com>
2019-07-12 15:20:02 -0400

    First use of flake8 and black

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template
+
 app = Flask("ConFi")
 
 
-@app.route('/')
+@app.route("/")
 def create():
-    data = { "title"   : "Wifi Registration",
-             "subtitle": "WiFi Registration",
-             "content" : render_template("create.tpl"),
-           }
-    return render_template("main.tpl",**data)
+    data = {
+        "title": "Wifi Registration",
+        "subtitle": "WiFi Registration",
+        "content": render_template("create.tpl"),
+    }
+    return render_template("main.tpl", **data)
 
-@app.route('/configure')
+
+@app.route("/configure")
 def configure():
-    data = { "title"   : "Configuration",
-             "subtitle": "WiFi Configuration",
-             "content" : render_template("configure.tpl"),
-           }
-    return render_template("main.tpl",**data)
+    data = {
+        "title": "Configuration",
+        "subtitle": "WiFi Configuration",
+        "content": render_template("configure.tpl"),
+    }
+    return render_template("main.tpl", **data)

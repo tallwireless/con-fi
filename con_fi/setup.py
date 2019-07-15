c61eb657227c553bf71ca129435f037f1a6e3103
Charles Rumford <charlesr@tallwireless.com>
2019-07-14 22:12:40 -0400

    Setup the database sessions

---
+++ con_fi/setup.py
@@ -0,0 +1,13 @@
+from . import db
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def setup(config):
+    engine = None
+    if config.DATABASE_TYPE == "sqlite":
+        db_type = "sqlite:///" + config.SQLITE_CONFIG["location"]
+        engine = create_engine(db_type, echo=config.DATABASE_ECHO)
+
+    db.SESSION = sessionmaker(bind=engine)

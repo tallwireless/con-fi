fbcad7fbb6db93c71719158c0a7fd60949b7d060
Charles Rumford <charlesr@isc.upenn.edu>
2019-07-21 16:14:55 -0400

    SQL

--- con_fi/entities.py
+++ con_fi/entities.py
@@ -1,6 +1,7 @@
 # All of the enities to be used
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
@@ -13,7 +14,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     username = Column(String(25))
-    password = Column(String(255))
 
     role_id = Column(Integer, ForeignKey("roles.id"))
 
@@ -21,7 +21,18 @@ class User(Base):
 
     api_key = relationship("API_Key", back_populates="user")
 
+    # Attributes are stored in the JSON column type. The key is attribute
+    # name. Every attribute has a op and value associated with it.
+#    SELECT id, username, json_array_elements(info)->>'attribute' as attribute, 
+#                         json_array_elements(info)->>'op' as op, 
+#                         json_array_elements(info)->>'value'
+#                as value FROM public.users where id=7;
+    attributes = Column(JSON, default = {})
+    
+
     enabled = Column(Boolean, default=True)
+
+
 
 
 class Role(Base):

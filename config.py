c9686e1cb0d32d2a28dadfaca8b772c44ae05a90
Charles Rumford <charlesr@tallwireless.com>
2019-07-30 10:52:10 -0400

    Adding the App Key

--- config.py
+++ config.py
@@ -19,6 +19,12 @@ SQLITE_CONFIG = {
     "location": "env/db.sqlite"
 }
 
+#########
+#
+#  Postgres Settings
+#
+#########
+
 POSTGRES_CONFIG = {
     "username": "dev",
     "password": "dev",
@@ -26,3 +32,15 @@ POSTGRES_CONFIG = {
     "host": "127.0.0.1",
     "port": "5432",
 }
+
+############
+#
+# Catcha Settings
+#
+############
+
+# This is the key which is used to encrypt the captcha
+
+# THIS SHOULD BE CHANGED BEFORE DEPLOYMENT
+
+APP_KEY = "194e4669d1e6859d3898bec5a9d4e111b1b23afc"

a02c8e0c1cb004430a25f100651ee8e67339b6ee
Charles Rumford <charlesr@tallwireless.com>
2019-07-31 12:53:03 -0400

    Adding RADIUS related tables

--- con_fi/entities.py
+++ con_fi/entities.py
@@ -2,11 +2,37 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import JSON
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, TimeStamp
 from sqlalchemy import ForeignKey
 
 
 Base = declarative_base()
+
+
+class NAS(Base):
+    __tablename__ = "nas"
+
+    id = Column(Integer, primary_key=True)
+    nasname = Column(String, nullable=False, index=True)
+    shortname = Column(String, nullable=False)
+    type = Column(String, default="other", nullable=False)
+    ports = Column(Integer)
+    secret = Column(String, nullable=False)
+    server = Column(String)
+    community = Column(String)
+    description = Column(String)
+
+
+class PostAuth(Base):
+    __tablename__ = "radpostauth"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    pass1 = Column(String)
+    reply = Column(String)
+    CalledStationId = Column(String)
+    CallingStationId = Column(String)
+    authdate = Column(TimeStamp(timezone=True), nullable=False)
 
 
 class User(Base):
